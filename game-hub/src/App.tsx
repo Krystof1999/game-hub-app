76fde535626b4a3c04ffeeca18e2d3ce93854913
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-06-17 19:07:09 +0200

    init commit - grid layout

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <div className="navbar">NavBar</div>
+      <div className="aside">
+        <h1>Aside</h1>
+        <ul>1</ul>
+        <ul>2</ul>
+        <ul>3</ul>
+        <ul>4</ul>
+        <ul>5</ul>
+        <ul>6</ul>
+        <ul>6</ul>
+        <ul>6</ul>
+      </div>
+      <div className="header-section">HeaderSection</div>
+      <div className="games-section">GamesSection</div>
+    </div>
+  );
+}
+
+export default App;
