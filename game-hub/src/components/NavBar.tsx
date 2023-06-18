3206e392dc2b50a825aeb8dbb8051ff1039af198
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-06-18 08:36:50 +0200

    navbar update

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,15 +1,21 @@
+import { AiOutlineSearch } from "@react-icons/all-files/ai/AiOutlineSearch";
+
 const NavBar = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center">
       <div className="w-16 h-16">
         <img src="/src/assets/logo.webp" alt="navbar-logo" className=" " />
       </div>
-      <div>
-        <form>
-          <input type="text" className="" />
+      <div className="flex-grow bg-blue-200 rounded-full p-1">
+        <form className="flex w-full items-center">
+          <AiOutlineSearch color="white" size="23px" />
+          <input
+            type="text"
+            className="w-full bg-blue-200 border-0 outline-none"
+          />
         </form>
       </div>
-      <div>
+      <div className="flex">
         <p>Switch</p>
         <p>Dark mode</p>
       </div>
