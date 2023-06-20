a781658a28e34d9aea5aa68b07629a938eb58355
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-06-20 08:38:14 +0200

    dropdown menu update

--- game-hub/src/components/HeaderSectoin.tsx
+++ game-hub/src/components/HeaderSectoin.tsx
@@ -1,25 +1,36 @@
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
+import { MdKeyboardArrowDown } from "@react-icons/all-files/md/MdKeyboardArrowDown";
 
 const HeaderSectoin = () => {
   return (
-    <div>
-      <h1>Games</h1>
+    <div className="text-white bg-custom-black pt-4">
+      <h1 className="font-bold text-[40px]">Games</h1>
       <div className="flex">
         <div>
-          <Menu menuButton={<MenuButton>Menu1</MenuButton>} transition>
-            <MenuItem
-              value="Cut"
-              onClick={(e) => console.log(`[MenuItem] ${e.value} clicked`)}
-            >
-              Cut
-            </MenuItem>
-            <MenuItem>Copy</MenuItem>
-            <MenuItem>Paste</MenuItem>
+          <Menu
+            menuButton={
+              <MenuButton className="flex place-items-end bg-custom-light-black p-2 rounded-md">
+                Platforms
+                <MdKeyboardArrowDown size="18px" />
+              </MenuButton>
+            }
+            transition
+          >
+            <div className="text-white">
+              <MenuItem
+                value="Cut"
+                onClick={(e) => console.log(`[MenuItem] ${e.value} clicked`)}
+              >
+                Cut
+              </MenuItem>
+              <MenuItem>Copy</MenuItem>
+              <MenuItem>Paste</MenuItem>
+            </div>
           </Menu>
         </div>
-        <div>
+        {/* <div>
           <Menu menuButton={<MenuButton>Menu2</MenuButton>} transition>
             <MenuItem
               value="Cut"
@@ -29,7 +40,7 @@ const HeaderSectoin = () => {
             </MenuItem>
             <MenuItem>Copy</MenuItem>
           </Menu>
-        </div>
+        </div> */}
       </div>
     </div>
   );
