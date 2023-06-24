202f0c94955e14991ca2a68f3dd032fb89d9783c
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-06-24 21:28:46 +0200

    aside update

--- game-hub/src/components/Aside.tsx
+++ game-hub/src/components/Aside.tsx
@@ -11,17 +11,18 @@ const Aside = ({ onSelectedGenre }: Props) => {
     <div className="pl-4 bg-custom-black text-white pr-14">
       <h1 className=" text-[25px] font-semibold mb-4">Genres</h1>
       {data.map((genre) => (
-        <ul
-          className="flex items-center mb-4"
-          key={genre.id}
-          onClick={() => onSelectedGenre(genre)}
-        >
+        <ul className="flex items-center mb-4 " key={genre.id}>
           <img
             src={genre.image_background}
             className="rounded-md w-10 h-10 object-cover"
           />
 
-          <p className="pl-3">{genre.name}</p>
+          <p
+            className="pl-3 cursor-pointer hover:underline"
+            onClick={() => onSelectedGenre(genre)}
+          >
+            {genre.name}
+          </p>
         </ul>
       ))}
     </div>
