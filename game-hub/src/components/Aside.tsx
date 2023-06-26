79dd38699f73409e83a71daf66804ab1b783da6b
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-06-26 09:38:33 +0200

    shipping static data update

--- game-hub/src/components/Aside.tsx
+++ game-hub/src/components/Aside.tsx
@@ -1,17 +1,16 @@
 import useGenres, { Genre } from "../hooks/useGenres";
-import genres from "../data/genres.json";
 
 interface Props {
   onSelectedGenre: (genre: Genre) => void;
 }
 
 const Aside = ({ onSelectedGenre }: Props) => {
-  // const { data } = useGenres();
+  const { data } = useGenres();
 
   return (
     <div className="pl-4 bg-custom-black text-white pr-14">
       <h1 className=" text-[25px] font-semibold mb-4">Genres</h1>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <ul className="flex items-center mb-4 " key={genre.id}>
           <img
             src={genre.image_background}
