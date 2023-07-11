9996a2e8f51f9f99cc8848c072279529b4259f89
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-07-11 14:11:14 +0200

    implement infinite queries

--- game-hub/src/components/GamesSection.tsx
+++ game-hub/src/components/GamesSection.tsx
@@ -1,25 +1,40 @@
+import React from "react";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameBox from "./GameBox";
 import GameBoxSkeleton from "./GameBoxSkeleton";
 
 //TODO - Fix the bug
+//TODO - Add the sceleton for GameBox
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GamesSection = ({ gameQuery }: Props) => {
-  const { data, isLoading } = useGames(gameQuery);
-  console.log(isLoading);
+  const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useGames(gameQuery);
 
   return (
     <>
       <div className="grid grid-cols-4">
-        {!isLoading
+        {/* {!isLoading
           ? data?.results.map((game) => <GameBox key={game.name} game={game} />)
-          : data?.results.map((game) => <GameBoxSkeleton key={game.name} />)}
+          : data?.results.map((game) => <GameBoxSkeleton key={game.name} />)} */}
+
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameBox key={game.name} game={game}></GameBox>
+            ))}
+          </React.Fragment>
+        ))}
       </div>
+      {hasNextPage && (
+        <button className="bg-red-200" onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? "Loading..." : "Load More"}
+        </button>
+      )}
     </>
   );
 };
